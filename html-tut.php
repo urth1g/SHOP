c059210302e81c578355ba220f64ce0fb292ee5d
Djordje J <jevremovicdjordje97@gmail.com>
2020-07-07 18:15:44 +0200

    first commit

---
+++ html-tut.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>	Test </title>
+	<style>
+
+	</style>
+</head>
+<body>
+
+	<script>
+
+	</script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Shop</title>
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet"> 
+	<link href="./css/main.css" rel="stylesheet">
+</head>
+<body>
+	<div class="container-fluid cont">
+		<nav class="navigation">
+			<div class="row innerContainer">
+				<div class="col-sm-2 c1">
+					LOGO
+				</div>
+
+				<div class="col-sm-4 offset-sm-6 c2">
+					<a href="/home">Home</a>
+					<a href="/products">Products</a>
+					<a href="/sign-in">Sign in</a>
+				</div>
+			</div>
+		</nav>
+
+		<div class="col-sm-12" style="padding:0; position:relative;">
+			<div class="hero">
+				<img src="http://via.placeholder.com/1920x1080" style="max-width: 100%"/>
+				<button class="btn btn-success">Shop</button>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
